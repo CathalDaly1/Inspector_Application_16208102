caeda48e7efcfac0d198ceb6dcf570b493216546
cathaldaly1 <16208102@studentmail.ul.ie>
2020-04-12 20:45:40 +0100

    Fully implemented the email system in the application

--- InspectorAdditionalFunctionality/emailSystem.py
+++ InspectorAdditionalFunctionality/emailSystem.py
@@ -47,22 +47,11 @@ def emailSystem():
         """
         assignmentCombobox()
 
-    def displayModuleAssignments():
-        """
-        This class retrieves assignment information from the assignments table in the database.
-        """
-        cur.execute("SELECT * FROM assignments WHERE user_id =%s and modulecode=%s",
-                    (userID, moduleCodeSelection))
-
-        assignmentData = cur.fetchall()
-        print(assignmentData)
-        conn.commit()
-
     def assignmentCombobox():
         """
         This docsting must be filled in
         """
-        global moduleCodeSelection
+        global moduleCodeSelection, assignmentSelection, assignmentCombo
         moduleCodeSelection = moduleCombobox.get()
         label_assignment = tk.Label(window, text="Choose Assignment No.: ", font=("Calibri", 14))
         label_assignment.place(x=25, y=125)
@@ -76,29 +65,54 @@ def emailSystem():
         assignmentCombo = ttk.Combobox(window, values=assignmentSelection)
         assignmentCombo.place(x=230, y=130)
 
-        saveAssignmentSelection = tk.Button(window, text="Display Table", fg="black", command=showTable, width=15)
+        saveAssignmentSelection = tk.Button(window, text="Save selection", fg="black", command=showTable, width=15)
         saveAssignmentSelection.place(x=400, y=127)
+
+    def displayModuleAssignments():
+        """
+        This class retrieves assignment information from the assignments table in the database.
+        """
+        test = tk.Label(window, text="Module code: " + str(moduleCodeSelection) + " and Assignment number: " +
+                                     str(assignmentSelect) + " saved", font=("Calibri", 14))
+        test.place(x=25, y=160)
 
     def showTable():
         """
         This docsting must be filled in
         """
-        global moduleCodeSelection
+        global moduleCodeSelection, assignmentSelect
         moduleCodeSelection = moduleCombobox.get()
+        assignmentSelect = assignmentCombo.get()
         displayModuleAssignments()
 
+    # ToDo implement with tkinter and also implement attachments, not sure how I will do that
     def send_email():
+        """
+        This method will allow the user to send emails to students with attachment and grade.
+        """
         try:
             email_user = '16208102@studentmail.ul.ie'
-            email_send = 'cathald96@gmail.com'
+            email_password = ''
+            email_send = ['cathald96@gmail.com', 'cathald96@gmail.com', 'cathald96@gmail.com']
+
+            subject = emailSubjectEntry.get('1.0', 'end-1c')
+
+            msg = MIMEMultipart()
+            msg['From'] = email_user
+            msg['To'] = ",".join(email_send)
+            msg['Subject'] = subject
+
             server = smtplib.SMTP('smtp-mail.outlook.com')
             server.starttls()
-            server.login(email_user, '')
+            server.login(email_user, email_password)
 
-            message = 'This is an email'
-            server.sendmail(email_user, email_send, message)
+            body = emailBodyEntry.get('1.0', 'end-1c')
+            msg.attach(MIMEText(body, 'plain'))
+
+            text = msg.as_string()
+            server.sendmail(email_user, email_send, text)
             server.quit()
-            print("email has been sent to " + email_send)
+            print("email has been sent!")
 
         except Exception as error:
             print(str(error))
@@ -110,12 +124,24 @@ def emailSystem():
         """
         window.destroy()
 
-    sendEmailButton = tk.Button(window, text="Send email", fg="black", command=send_email,
-                                    width=15)
-    sendEmailButton.place(x=400, y=150)
-
     saveModuleSelection = tk.Button(window, text="Display Assignments", fg="black", command=displayAssignment, width=15)
     saveModuleSelection.place(x=400, y=100)
+
+    emailSubject_lbl = tk.Label(window, text="Email Subject: ", font=("Calibri", 14))
+    emailSubject_lbl.place(x=25, y=200)
+
+    emailSubjectEntry: tk.Text = tk.Text(window, height="2", width="60")
+    emailSubjectEntry.place(x=160, y=205)
+
+    emailBody_lbl = tk.Label(window, text="Email Body: ", font=("Calibri", 14))
+    emailBody_lbl.place(x=25, y=260)
+
+    emailBodyEntry: tk.Text = tk.Text(window, height="10", width="60")
+    emailBodyEntry.place(x=160, y=265)
+
+    sendEmailButton = tk.Button(window, text="Send emails", fg="black", command=send_email,
+                                width=15)
+    sendEmailButton.place(x=400, y=600)
 
     back_button = tk.Button(window, text="Back", fg="black", command=back, height=2, width=12)
     back_button.place(x=350, y=730)
